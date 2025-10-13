8588f6756d0c4257ce56ccfa799069a6fa6ed437
Brian Blackmore <blb8@po.cwru.edu>
2025-10-13 09:53:20 -0700

    merge Attribute updates in master: * ae7c627 Attributes push/pop, dump/restore * 391eb22 Attribute prevent value update from historic changes

--- lib/Schedule/Activity/Attribute.pm
+++ lib/Schedule/Activity/Attribute.pm
@@ -25,6 +25,7 @@ sub new {
 		type =>$opt{type}//'int',
 		value=>$opt{value}//0,
 		log  =>{},
+		tmmax=>$opt{tm}//0,
 	);
 	if(!defined($types{$self{type}})) { die "Attribute invalid type:  $self{type}" }
 	$self{log}{$opt{tm}//0}=$self{value};
@@ -41,20 +42,24 @@ sub validateConfig {
 
 sub log {
 	my ($self,$tm)=@_;
-	if(defined($tm)) { $$self{log}{$tm}=$$self{value} }
+	if(defined($tm)&&($tm>=$$self{tmmax})) { $$self{log}{$tm}=$$self{value}; $$self{tmmax}=$tm }
 	return $self;
 }
 
 sub change {
-	my ($self,@opt)=@_;
-	return &{$types{$$self{type}}{change}}($self,@opt);
+	my ($self,%opt)=@_;
+	my $tm=$opt{tm}//$$self{tmmax};
+	if($tm<$$self{tmmax}) { return $self }
+	&{$types{$$self{type}}{change}}($self,%opt);
+	$self->log($tm); # updates tmmax
+	return $self;
 }
 
 sub report {
 	my ($self)=@_;
 	return (
 		y  =>$$self{value},
-		xy =>[$self->xy()],
+		xy =>[$self->_xy()],
 		avg=>$self->average(),
 	);
 }
@@ -70,29 +75,33 @@ sub average {
 	return &{$types{$$self{type}}{average}}($$self{log});
 }
 
-sub xy {
-	my ($self)=@_;
-	return map {[$_,$$self{log}{$_}]} sort {$a<=>$b} keys %{$$self{log}};
-}
-
 sub dump {
 	my ($self)=@_;
 	my %res=(
-		type=>$$self{type},
-		value=>$$self{value},
 		log=>{ %{$$self{log}} },
+		(map {$_=>$$self{$_}} qw/type value tmmax/),
 	);
 	return %res;
 }
 
 sub restore {
-	my ($class,%opt)=@_;
+	my ($ref,%opt)=@_;
+	if(ref($ref)) {
+		foreach my $k (keys %opt) { $$ref{$k}=$opt{$k} }
+		return $ref;
+	}
 	my %self=(
 		type =>$opt{type}//'int',
 		value=>$opt{value}//0,
 		log  =>$opt{log}//{},
+		tmmax=>$opt{tmmax}//0,
 	);
-	return bless(\%self,$class);
+	return bless(\%self,$ref);
+}
+
+sub _xy {
+	my ($self)=@_;
+	return map {[$_,$$self{log}{$_}]} sort {$a<=>$b} keys %{$$self{log}};
 }
 
 # set=>value
@@ -104,7 +113,6 @@ sub _changeInt {
 	if(defined($opt{set})) { $$self{value}=$opt{set} }
 	if($opt{incr})         { $$self{value}+=$opt{incr} }
 	if($opt{decr})         { $$self{value}-=$opt{decr} }
-	if(defined($opt{tm}))  { $self->log($opt{tm}) }
 	return $self;
 }
 
@@ -113,7 +121,6 @@ sub _changeInt {
 sub _changeBool {
 	my ($self,%opt)=@_;
 	if(defined($opt{set})) { $$self{value}=$opt{set} }
-	if(defined($opt{tm}))  { $self->log($opt{tm}) }
 	return $self;
 }
 
@@ -151,8 +158,70 @@ __END__
 
 =pod
 
+=head1 NAME
+
+Schedule::Activity::Attribute - Updating, tracking, and reporting numeric values.
+
+=head1 SYNOPSIS
+
+  my $attr=Schedule::Activity::Attribute->new(type=>'int',value=>0,tm=>0);
+  $attr->change(tm=>10,set=>10);
+  $attr->change(tm=>20,set=>20);
+  print $attr->value();
+  print $attr->average();
+
+=head1 DESCRIPTION
+
 This module is responsible for individual attributes.  It tracks values for the attribute, facilitates updates, and handles reporting.
 
-Note that attributes are intended to be I<typed>, so, for example, during the configuration phase, if there is a request for C<name=boolean> and for C<name=integer>, there should be a failure.
+Attributes are intended to be I<typed>, so cross-type requests may produce failures.
+
+=head1 Functions
+
+=head2 change
+
+Ideally called as C<$attr->change(tm=>tm,options)>, this updates the value of the attribute and logs at the given timestamp.  Change options must be type-appropriate.  The specified time must exceed (or be the same as) the maximum logged time for the attribute to have any effect.  That is, historical values don't update the current value, nor do they create an entry in the log.
+
+Called without a timestamp, the maximum time will be assumed, the value changed, and the logged entry overwritten.
+
+Historic entry is proposed by not yet defined, since it must support C<set> and C<incr> options.
+
+=head2 value
+
+The value of the attribute associated with the logged event having the maximum time.
+
+=head2 average
+
+Computes the type-specific time-weighted "average value" over all entries in the log.  Currently this is a heavy action that requires recalculation for each call, making it accurate but slow.
+
+=head2 report
+
+Returns a hash of
+
+  y    the value
+  xy   this list of logged (time,value)
+  avg  the time-weighted average
+
+=head2 C<_xy>
+
+An internal helper to build the list of values from the log.
+
+=head2 validateConfig
+
+Returns errors if any of the keys in an attribute configuration are unavailable for the type.  By default, the type is 'int'.
+
+=head2 log
+
+Called with a timestamp as C<$attr->log(tm)> to create a log entry of the current value.  This is public so callers can establish a "checkpoint" where the value is known/unchanged, typically at boundaries of events or scheduling windows.
+
+Does nothing if the indicated time is below the maximum logged time.
+
+=head2 dump
+
+Return a hash of values that can be used to restore the state of the attribute.
+
+=head2 restore
+
+Called either as a static function or C<$attr->restore(%copy)>, loads the state of the attribute from the given hash.
 
 =cut
